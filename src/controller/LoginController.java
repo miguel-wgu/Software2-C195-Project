d8c9413a4ba54ffaf9e1e16c80e66e5a49267083
Miguel Guzman <53159738+mag-projects@users.noreply.github.com>
2023-03-26 18:16:48 -0700

    Updated Login Controller to create/update login text file and updated javadocs for it

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -10,40 +10,86 @@ import javafx.scene.control.TextField;
 import utils.ErrMsg;
 import utils.HelperFunctions;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.net.URL;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 /**
+ * Controller for LoginScene.fxml
+ *
  * @author Miguel Guzman
+ *
+ * <br><br>
+ * This controller is responsible for validating the user login credentials.
  */
 public class LoginController implements Initializable {
 
+	/**
+	 * Constant to hold the login log file path.
+	 */
+	private static final String LOGIN_FILE_PATH = "./login_log.txt";
+	/**
+	 * Variable to hold the username.
+	 */
+	private static String userName;
+	/**
+	 * The username field.
+	 */
 	@FXML
 	private TextField userNameTextField;
+	/**
+	 * The password field.
+	 */
 	@FXML
 	private PasswordField loginPasswordField;
+	/**
+	 * The login button.
+	 */
 	@FXML
 	private Button loginBtn;
+	/**
+	 * The clear button.
+	 */
 	@FXML
 	private Button clearLoginBtn;
+	/**
+	 * Label displays app name.
+	 */
 	@FXML
 	private Label schedulerLoginLabel;
+	/**
+	 * Label displays timezone text
+	 */
 	@FXML
 	private Label timezoneLabel;
+	/**
+	 * Label displays current location text
+	 */
 	@FXML
 	private Label currentLocationLabel;
 
-
-	private static String userName;
-
+	/**
+	 * Gets username.
+	 *
+	 * @return the username
+	 */
 	@FXML
 	public static String getUserName() {
 		return userName;
 	}
 
-
+	/**
+	 * Initializes the login scene with the current timezone and language.
+	 * @param url the url
+	 * @param resourceBundle the resource bundle
+	 */
 	@Override
 	public void initialize(URL url, ResourceBundle resourceBundle) {
 		ZoneId zoneId = ZoneId.systemDefault();
@@ -61,10 +107,12 @@ public class LoginController implements Initializable {
 	 * Login btn click.
 	 *
 	 * @param actionEvent the action event
+	 * @throws SQLException the sql exception
 	 */
-	public void loginBtnClick(ActionEvent actionEvent) {
+	public void loginBtnClick(ActionEvent actionEvent) throws SQLException {
 		userName = userNameTextField.getText();
 		String password = loginPasswordField.getText();
+		logLogin(userName, password);
 
 		// check for empty string, if false start MainScene
 		try {
@@ -74,6 +122,7 @@ public class LoginController implements Initializable {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 	}
 
 	/**
@@ -82,5 +131,36 @@ public class LoginController implements Initializable {
 	public void clearLoginBtnClick() {
 		userNameTextField.setText("");
 		loginPasswordField.setText("");
+	}
+
+	/**
+	 * Creates/updates the login log file with the user login attempt.
+	 * @param userName the username
+	 * @param password the password
+	 * @throws SQLException if there is an error with the database
+	 */
+	private static void logLogin(String userName, String password) throws SQLException {
+		boolean attempt = (!ErrMsg.isEmptyField(userName, password) && !ErrMsg.isIncorrect(userName, password));
+		try {
+			LocalDateTime local = LocalDateTime.now(ZoneOffset.UTC);
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd, yyyy - HH:mm:ss");
+			String formattedTime = local.format(formatter);
+			File logFile = new File(LOGIN_FILE_PATH);
+			if (!logFile.exists()) {
+				boolean wasFileCreated = logFile.getParentFile().mkdirs();
+				if (wasFileCreated) {
+					wasFileCreated = logFile.createNewFile();
+					if (!wasFileCreated)
+						throw new RuntimeException("Could not create file: " + logFile.getAbsolutePath());
+					else
+						throw new RuntimeException("Could not create directory: " + logFile.getParentFile().getAbsolutePath());
+				}
+			}
+			try (FileWriter writer = new FileWriter(logFile, true)) {
+				writer.write("User " + userName + " " + (attempt ? "successfully" : "unsuccessfully") + " logged in: " + formattedTime + " UTC\n");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
